acf6e67bf79fa8cdc7606965e6b4bd387166027a
Volker Brömmel <wbommel@users.noreply.github.com>
2019-01-16 12:53:10 +0100

    early development

---
+++ AdfCommanderLib.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AdfCommanderLib.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Licence Owner")]
+[assembly: AssemblyProduct("AdfCommanderLib.test")]
+[assembly: AssemblyCopyright("Copyright © Licence Owner 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e22c9f5a-60f4-49e3-a243-8cf8d7c04a57")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AdfCommanderLib/AdfFile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdfCommanderLib
+{
+    public class AdfFile
+    {
+        #region ***** declarations
+        private string _strFilename = string.Empty;
+
+        private byte[] _bytMain = new byte[901119];
+        private byte[] _bytCompare = new byte[901119];
+        #endregion
+
+        #region ***** constructor / terminator / disposer
+        public AdfFile()
+        {
+            _initAdfFile();
+        }
+        public AdfFile(string fileName) : this()
+        {
+            _strFilename = fileName;
+        }
+        #endregion
+
+        #region ***** private functions
+        private void _initAdfFile()
+        {
+            if (!File.Exists(_strFilename))
+            {
+                return;
+            }
+        }
+        #endregion
+
+        #region ***** methods
+        #endregion
+
+        #region ***** properties
+        #endregion
+    }
+}
