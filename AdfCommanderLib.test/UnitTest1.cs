0da2c5fbfc0f454b1466d9595e622c7b4f2bc3d9
Bommel <wbommel@web.de>
2019-01-19 19:36:42 +0100

    Early Development

--- AdfCommanderLib.test/UnitTest1.cs
+++ AdfCommanderLib.test/UnitTest1.cs
@@ -9,6 +9,24 @@ namespace AdfCommanderLib.test
         [TestMethod]
         public void TestMethod1()
         {
+            try
+            {
+                var t = new AdfFile(@"u:\Emulation\Amiga\WinUAE4100_x64\DiskImages\games\HLS_NHL_2018_2019 - Kopie.adf");
+                Assert.IsTrue(true);
+            }
+            catch (Exception e)
+            {
+                Assert.IsFalse(true, e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ReadSector()
+        {
+            var t = new AdfFile(@"u:\Emulation\Amiga\WinUAE4100_x64\DiskImages\games\HLS_NHL_2018_2019 - Kopie.adf");
+            var bytSector = t.GetSector(0);
+
+            Assert.IsTrue(bytSector.Length == 512, System.Text.Encoding.ASCII.GetString(bytSector, 0, 25));
         }
     }
 }
--- AdfCommanderLib/AdfFile.cs
+++ AdfCommanderLib/AdfFile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,32 +13,68 @@ namespace AdfCommanderLib
         #region ***** declarations
         private string _strFilename = string.Empty;
 
-        private byte[] _bytMain = new byte[901119];
-        private byte[] _bytCompare = new byte[901119];
-        #endregion
+        private byte[] _bytMain = new byte[901120];
+        private byte[] _bytCompare = new byte[901120];
+        #endregion  
 
         #region ***** constructor / terminator / disposer
         public AdfFile()
         {
-            _initAdfFile();
+
         }
         public AdfFile(string fileName) : this()
         {
             _strFilename = fileName;
+            _initAdfFile();
         }
         #endregion
 
         #region ***** private functions
         private void _initAdfFile()
         {
+            //check prerequisites
             if (!File.Exists(_strFilename))
             {
                 return;
             }
+
+            try
+            {
+                using (var fsSource = new FileStream(_strFilename, FileMode.Open, FileAccess.Read))
+                {
+                    //not the right size
+                    if (fsSource.Length != 901120) { return; }
+
+                    //read file to buffers
+                    fsSource.Read(_bytMain, 0, 901120);
+                    fsSource.Seek(0, SeekOrigin.Begin);
+                    fsSource.Read(_bytCompare, 0, 901120);
+                    fsSource.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private byte[] _getSector(int sectorNumber)
+        {
+            var bytSector = new byte[512];
+
+            Buffer.BlockCopy(_bytMain, sectorNumber * 512, bytSector, 0, 512);
+
+            return bytSector;
         }
         #endregion
 
         #region ***** methods
+
+        public byte[] GetSector(int sectorNumber)
+        {
+            return _getSector(sectorNumber);
+        }
         #endregion
 
         #region ***** properties
