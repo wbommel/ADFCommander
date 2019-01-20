faf1556cad8b8977db9af06d609f2285eecd77e5
Volker Brömmel <wbommel@web.de>
2019-01-20 11:08:04 +0100

    first implementation of HLS DOS
    created a HlsDos.cs which uses the AdfFile.cs class to read the relevant sectors of an adf file. HLS is the Amiga "Hockey Leage Simulator" for the "Wayne Gretzky Hockey" games. Unfortunately the developers created an own DOS format for their save discs.
    This first implementation will be able to read the directory of the save disc.

--- AdfCommanderLib/HlsDos.cs
+++ AdfCommanderLib/HlsDos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace AdfCommanderLib
     public class HlsDos
     {
         #region ***** declarations
+        private AdfFile _adfFile;
         #endregion
 
         #region ***** constructor / terminator / disposer
@@ -18,6 +20,7 @@ namespace AdfCommanderLib
             if (adfFile == null) { throw new ArgumentException("No adf file object supplied."); }
             if (!adfFile.FileLoaded) { throw new ArgumentException("No adf file loaded."); }
 
+            _adfFile = adfFile;
         }
 
         #region dispose pattern (http://www.codeproject.com/Articles/15360/Implementing-IDisposable-and-the-Dispose-Pattern-P)
@@ -45,6 +48,63 @@ namespace AdfCommanderLib
         #endregion
 
         #region ***** private functions
+        private string _getDirectory()
+        {
+            /* The directory is contained in the first 10 sectors
+             * starting at byte 42 with an entry siz eof 46 bytes */
+            var bytDir = _adfFile.GetSectors(0, 10);
+            var intStartOfDirectory = 42;
+            var intEntrySize = 46;
+            StringBuilder sbHex = new StringBuilder();
+            StringBuilder sbText = new StringBuilder();
+
+            //read directory
+            int idx = intStartOfDirectory; //first directory entry
+            while (bytDir[idx] != 0)
+            {
+                //get entry
+                byte[] bytEntry = new byte[intEntrySize];
+                Buffer.BlockCopy(bytDir, idx, bytEntry, 0, intEntrySize);
+
+                //create hex representation
+                for (int i = 0; i <= bytEntry.GetUpperBound(0); i++)
+                {
+                    if (i == 44) { }
+                    sbHex.Append(bytEntry[i].ToString("X2"));
+                    sbHex.Append(" ");
+                }
+                sbHex.Append(Environment.NewLine);
+
+                //create text representation
+                //Console.WriteLine(System.Text.Encoding.ASCII.GetString(bytEntry));
+                sbText.Append(Encoding.ASCII.GetString(bytEntry).Substring(0, Encoding.ASCII.GetString(bytEntry).IndexOf((char)0)));
+                sbText.Append(Environment.NewLine);
+
+                //output to trace
+                Trace.WriteLine(sbHex.ToString());
+                Trace.WriteLine(sbText.ToString());
+
+                //goto next entry
+                idx += intEntrySize;
+            }
+
+            return sbHex.ToString();
+        }
+
+        private uint _swapEndianness(uint x)
+        {
+            return ((x & 0x000000ff) << 24) +  // First byte
+                   ((x & 0x0000ff00) << 8) +   // Second byte
+                   ((x & 0x00ff0000) >> 8) +   // Third byte
+                   ((x & 0xff000000) >> 24);   // Fourth byte
+        }
+        #endregion
+
+        #region ***** methods
+        public string GetDirectory()
+        {
+            return _getDirectory();
+        }
         #endregion
     }
 }
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AdfCommanderLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,25 +11,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            var t = new AdfCommanderLib.AdfFile(@"HLS_NHL_2018_2019 - Kopie.adf");
+            var t = new HlsDos(new AdfFile(@"HLS_NHL_2018_2019 - Kopie.adf"));
 
-            if (!t.FileLoaded) { return; }
+            Console.WriteLine(t.GetDirectory());
 
-            var bytDir = t.GetSectors(0, 10);
-
-
-            //read directory
-            int idx = 42;//first directory entry
-            while (bytDir[idx] != 0)
-            {
-                byte[] bytEntry = new byte[46];
-                Buffer.BlockCopy(bytDir, idx, bytEntry, 0, 46);
-
-                Console.WriteLine(System.Text.Encoding.ASCII.GetString(bytEntry));
-
-                idx += 46;
-
-            }
 
 
             Console.Write("press any key to end.");
